41972762afeae9ae54b3183f5668cd9e6121c2ce
yuchen10 <87979301+yuchen10@users.noreply.github.com>
2022-04-25 22:45:09 +0800

    ssss

---
+++ src/main/java/com/example/herokudemo/HerokudemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.herokudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HerokudemoApplication {
+    @GetMapping("/")
+    public String hello(){
+        return "hello121 world";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(HerokudemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/herokudemo/HerokudemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokudemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
